12d162c48f85be3226dd76bad4ac25680288a974
vorobeyAndrey <ptaha005@gmail.com>
2021-05-14 00:36:25 +0300

    add user api

--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -2,7 +2,7 @@ const uuid = require('uuid');
 
 class User {
   constructor({
-    id = uuid(),
+    id = uuid.v4(),
     name = 'USER',
     login = 'user',
     password = 'P@55w0rd'
@@ -11,6 +11,12 @@ class User {
     this.name = name;
     this.login = login;
     this.password = password;
+  }
+
+  update(user) {
+    this.name = user.name;
+    this.login = user.login;
+    this.password = user.password;
   }
 
   static toResponse(user) {
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,9 @@
 const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
+const getById = (userId) => usersRepo.getById(userId);
+const create = (user) => usersRepo.create(user);
+const update = (userId, user) => usersRepo.update(userId, user);
+const deleteUser = (userId) => usersRepo.deleteUser(userId);
 
-module.exports = { getAll };
+module.exports = {  deleteUser, update, create, getById, getAll };
