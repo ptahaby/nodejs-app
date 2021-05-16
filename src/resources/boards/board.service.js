06069f4e21e5693dd880ff10fb719de74c6ee0b2
vorobeyAndrey <ptaha005@gmail.com>
2021-05-16 20:58:25 +0300

    delete all task for board, put userId=null

--- src/resources/boards/board.service.js
+++ src/resources/boards/board.service.js
@@ -1,9 +1,13 @@
 const boardRepo = require('./board.memory.reporitory');
+const taskService = require('../tasks/task.service');
 
 const getAll = () => boardRepo.getAll();
 const getById = (id) => boardRepo.getById(id);
 const create = (board) => boardRepo.create(board);
 const update = (id, board) => boardRepo.update(id, board);
-const deleteBoard = (id) => boardRepo.deleteBoard(id) 
+const deleteBoard = (id) => {
+  taskService.deleteTasksByBoardId(id);
+  return boardRepo.deleteBoard(id);
+} 
 
 module.exports = { getAll, getById, create, update, deleteBoard };
