d0a93136e03c8190604a9524f910aa05d277fa47
vorobeyAndrey <ptaha005@gmail.com>
2021-05-30 20:06:26 +0300

    add users docs

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,23 +1,46 @@
 const User  = require('./user.model')
 
+/**
+ * @const {Map<string, User>}
+ */
 const users = new Map();
 
+/**
+ * Get all users
+ * @returns {Promise<Array<User>>} users
+ */
 const getAll = async () => { 
   const userList = Array.from(users.values());
   return userList;
 };
 
+/**
+ * Get user by id
+ * @param {string} id first term
+ * @returns {Promise<User>} user
+ */
 const getById = async (id) => {
   const user = users.get(id);
   return user;
 }
 
+/**
+ * Create user
+ * @param {{name: string, login: string, password: string}} data first term
+ * @returns {Promise<User>} user
+ */
 const create = async (data) => {
   const user = new User(data);
   users.set(user.id, user);
   return user;
 }
 
+/**
+ * Update the user
+ * @param {string} userId first term
+ * @param {{name: string, login: string, password: string}} data second term
+ * @returns {Promise<User>} user
+ */
 const update = async (userId, data) => {
   const user = users.get(userId);
   user.update(data);
@@ -25,6 +48,11 @@ const update = async (userId, data) => {
   return user;
 }
 
+/**
+ * Delete the user
+ * @param {string} userId first term
+ * @returns {Promise<boolean>} boolean
+ */
 const deleteUser = async(userId) => {
   const isDeleted = users.delete(userId);
   return isDeleted;
--- src/resources/users/user.model.js
+++ src/resources/users/user.model.js
@@ -1,6 +1,13 @@
 const uuid = require('uuid');
 
+/**
+ * Class representing a User
+ */
 class User {
+  /**
+   * Create a User
+   * @param {{id: string, name: string, login: string, password: string}} param0 first term
+   */
   constructor({
     id = uuid.v4(),
     name = 'USER',
@@ -13,12 +20,20 @@ class User {
     this.password = password;
   }
 
+  /**
+   * Update the user
+   * @param {{name: string, login: string, password: string}} user  first term
+   */
   update(user) {
     this.name = user.name;
     this.login = user.login;
     this.password = user.password;
   }
-
+  
+  /**
+   * Parse user to response
+   * @param {User} user first term
+   */
   static toResponse(user) {
     const { id, name, login } = user;
     return { id, name, login };
