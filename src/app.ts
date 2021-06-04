a7b5115a89b4a81b06976a7b1fe5afc4d6bb2927
vorobeyAndrey <ptaha005@gmail.com>
2021-06-04 18:31:57 +0300

    uncaughtException, unhandledRejection, add middleware

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,27 @@ import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
+import fs from 'fs';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
+import { logRequest, logErrorHandler, getFormattedDate } from './middleware/logger';
+
+process.on('uncaughtException', (error: Error) => {
+  const log = `[${getFormattedDate()}] UNCAUGHT EXCEPTION: ${error.stack || error.message}`;
+
+  fs.appendFile(`${__dirname  }/../../logs.txt`, `${log  }\n`, (err) => {
+    if(err) throw err;
+  });
+})
+
+process.on('unhandledRejection', (error: Error) => {
+  const log = `[${getFormattedDate()}] UNHANDLED REJECTION: ${error.stack || error.message}`;
+
+  fs.appendFile(`${__dirname  }/../../logs.txt`, `${log  }\n`, (err) => {
+    if(err) throw err;
+  });
+})
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -13,6 +31,7 @@ app.use(express.json());
 
 app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
+app.use(logRequest);
 app.use('/', (req: Request, res: Response, next: NextFunction) => {
   if (req.originalUrl === '/') {
     res.send('Service is running!');
@@ -24,5 +43,6 @@ app.use('/', (req: Request, res: Response, next: NextFunction) => {
 app.use('/users', userRouter);
 app.use('/boards', boardRouter);
 app.use('/boards', taskRouter);
+app.use(logErrorHandler);
 
 export default  app;
