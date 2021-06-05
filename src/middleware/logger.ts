5d40045a2a6ca0e3632c8e22c71212d8c139ffc7
vorobeyAndrey <ptaha005@gmail.com>
2021-06-05 15:41:23 +0300

    feractor

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,10 +1,15 @@
 import { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 
-export const getFormattedDate = (): string => {
+const getFormattedDate = (): string => {
     const currentDateTime = new Date();
     const formattedDate = `${currentDateTime.getFullYear()}-${currentDateTime.getMonth() + 1}-${currentDateTime.getDate()} ${currentDateTime.getHours()}:${currentDateTime.getMinutes()}:${currentDateTime.getSeconds()}`
     return formattedDate;
+}
+const addLogsToFile = (logs: string) => {
+  fs.appendFile(`${__dirname  }/../../logs.txt`, `${logs  }\n`, (err) => {
+    if(err) throw err;
+  });
 }
 
 export const logRequest = (req: Request, res: Response, next: NextFunction): void => {
@@ -13,21 +18,26 @@ export const logRequest = (req: Request, res: Response, next: NextFunction): voi
     const formattedBody = JSON.stringify(body);
     const formattedParams = JSON.stringify(query);
     const log = `[${getFormattedDate()}] ${method}:${url} ${statusCode}; Query params: ${formattedParams}; Body: ${formattedBody}`;
-
-    fs.appendFile(`${__dirname  }/../../logs.txt`, `${log  }\n`, (err) => {
-        if(err) throw err;
-    })
+    addLogsToFile(log);
     next();
 }
 
 export const logErrorHandler = (error: Error, _: Request, res: Response, next: NextFunction): void => {
     if(error instanceof Error) {
         const log = `[${getFormattedDate()}] Error: ${error.stack}`
-        fs.appendFile(`${__dirname  }/../../logs.txt`, `${log }\n`, (err) => {
-            if(err) throw err;
-        })
+        addLogsToFile(log);
         res.status(500).send('Something broke');
     } else {
         next();
     }
+}
+
+export const unhandledRejection =  (error: Error): void => {
+  const log = `[${getFormattedDate()}] UNHANDLED REJECTION: ${error.stack || error.message}`;
+  addLogsToFile(log);
+}
+
+export const uncaughtException = (error: Error): void => {
+  const log = `[${getFormattedDate()}] UNCAUGHT EXCEPTION: ${error.stack || error.message}`;
+  addLogsToFile(log);
 }
