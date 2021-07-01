15faa9570681ca72ef9413a5c30474cc85d77613
vorobeyAndrey <ptaha005@gmail.com>
2021-07-01 12:53:33 +0300

    update gitignore, loger

--- src/app.ts
+++ src/app.ts
@@ -2,30 +2,16 @@ import express, { Request, Response, NextFunction } from 'express';
 import swaggerUI from 'swagger-ui-express';
 import path from 'path';
 import YAML from 'yamljs';
-import fs from 'fs';
 import userRouter from './resources/users/user.router';
 import boardRouter from './resources/boards/board.router';
 import taskRouter from './resources/tasks/task.router';
 import loginRouter from './resources/login/login.router';
-import { logRequest, logErrorHandler, getFormattedDate } from './middleware/logger';
 import { verifyToken } from  './middleware/verify-session';
 import 'reflect-metadata';
+import { logRequest, logErrorHandler, uncaughtException, unhandledRejection } from './middleware/logger';
 
-process.on('uncaughtException', (error: Error) => {
-  const log = `[${getFormattedDate()}] UNCAUGHT EXCEPTION: ${error.stack || error.message}`;
-
-  fs.appendFile(`${__dirname  }/../../logs.txt`, `${log  }\n`, (err) => {
-    if(err) throw err;
-  });
-})
-
-process.on('unhandledRejection', (error: Error) => {
-  const log = `[${getFormattedDate()}] UNHANDLED REJECTION: ${error.stack || error.message}`;
-
-  fs.appendFile(`${__dirname  }/../../logs.txt`, `${log  }\n`, (err) => {
-    if(err) throw err;
-  });
-})
+process.on('uncaughtException', uncaughtException)
+process.on('unhandledRejection',unhandledRejection)
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,11 +1,20 @@
 import { Request, Response, NextFunction } from 'express';
 import fs from 'fs';
 
+const ERROR_LOGS = '/../../error-logs.txt';
+const REQUEST_LOGS = '/../../request-logs.txt';
+
 export const getFormattedDate = (): string => {
     const currentDateTime = new Date();
     const formattedDate = `${currentDateTime.getFullYear()}-${currentDateTime.getMonth() + 1}-${currentDateTime.getDate()} ${currentDateTime.getHours()}:${currentDateTime.getMinutes()}:${currentDateTime.getSeconds()}`
     return formattedDate;
 }
+
+const addLogsToFile = (logs: string, file: typeof ERROR_LOGS | typeof REQUEST_LOGS) => {
+    fs.appendFile(`${__dirname}${file}`, `${logs  }\n`, (err) => {
+      if(err) throw err;
+    });
+  }
 
 export const logRequest = (req: Request, res: Response, next: NextFunction): void => {
     const { url, method, body, query } = req
@@ -14,9 +23,7 @@ export const logRequest = (req: Request, res: Response, next: NextFunction): voi
     res.on('finish', () => {
         const { statusCode } = res;
         const log = `[${getFormattedDate()}] ${method}:${url} ${statusCode}; Query params: ${formattedParams}; Body: ${formattedBody}`;
-        fs.appendFile(`${__dirname  }/../../logs.txt`, `${log  }\n`, (err) => {
-            if(err) throw err;
-        })
+        addLogsToFile(log, REQUEST_LOGS);
     })
 
     next();
@@ -25,11 +32,19 @@ export const logRequest = (req: Request, res: Response, next: NextFunction): voi
 export const logErrorHandler = (error: Error, _: Request, res: Response, next: NextFunction): void => {
     if(error instanceof Error) {
         const log = `[${getFormattedDate()}] Error: ${error.stack}`
-        fs.appendFile(`${__dirname  }/../../logs.txt`, `${log }\n`, (err) => {
-            if(err) throw err;
-        })
+        addLogsToFile(log, ERROR_LOGS);
         res.status(500).send('Something broke');
     } else {
         next();
     }
 }
+
+export const unhandledRejection =  (error: Error): void => {
+    const log = `[${getFormattedDate()}] UNHANDLED REJECTION: ${error.stack || error.message}`;
+    addLogsToFile(log, ERROR_LOGS);
+  }
+  
+  export const uncaughtException = (error: Error): void => {
+    const log = `[${getFormattedDate()}] UNCAUGHT EXCEPTION: ${error.stack || error.message}`;
+    addLogsToFile(log, ERROR_LOGS);
+  } 
